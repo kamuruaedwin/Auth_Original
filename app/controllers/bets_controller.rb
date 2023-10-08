c8a9869d36916618a90b49a0c0768a79b77d8357
Edwin Njeru <kamuruaedwin@gmail.com>
2023-10-08 16:38:29 +0300

    bet logic updated

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,48 +1,40 @@
 # app/controllers/bets_controller.rb
 
 class BetsController < ApplicationController
+  before_action :set_user, only: [:create]
   def new
     @bet = Bet.new
   end
 
-  def update_outcomes(last_y_value)
-    Bet.where(outcome: nil).each do |bet|
-      outcome = bet.calculate_outcome(last_y_value)
-      bet.update(outcome: outcome)
-      current_user.balance += outcome
-      current_user.save
-    end
-  end
-
   def create
-  
+    # Simulate the animation's progress (replace this with your actual animation logic)
+    @bet = current_user.bets.build(bet_params)
+    
+    if @bet.save
+      # Calculate the outcome based on animation progress and predicted value
+      if animation_progress >= @bet.predicted_y_value
+        @bet.outcome = @bet.stake_amount * @bet.predicted_y_value
+        flash[:success] = "Congratulations you won!"
+      else
+        @bet.outcome = 0
+      end
 
-  # Simulate the animation's progress (replace this with your actual animation logic)
-  
-  @bet = current_user.bets.build(bet_params)
-  if @bet.save
-    # Calculate the outcome based on animation progress and predicted value
-    if animation_progress >= @bet.predicted_y_value
-      @bet.outcome = @bet.stake_amount * @bet.predicted_y_value
-      flash[:success] = "Congratulations you won!"
+      # Update the user's balance based on the outcome
+      current_user.balance += @bet.outcome
+      current_user.save
+
+      flash[:success] = "Bet placed successfully!"
+      redirect_to root_path
     else
-      @bet.outcome = 0
+      render :new
     end
-
-    # Update the user's balance based on the outcome
-    current_user.balance += @bet.outcome
-    current_user.save
-
-    flash[:success] = "Bet placed successfully!"
-    redirect_to root_path
-  else
-    render :new
   end
-end
 
+  private 
 
-
-  private
+  def set_user
+    @user = current_user
+  end
 
   def bet_params
     params.require(:bet).permit(:stake_amount, :predicted_y_value)
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -7,18 +7,20 @@ class Bet < ApplicationRecord
   validates :predicted_y_value, presence: true, numericality: { greater_than_or_equal_to: 8 }
   validate :stake_amount_less_than_or_equal_to_balance
 
-  def stake_amount_less_than_or_equal_to_balance
-    if @bet.stake_amount > user.balance
-      errors.add(:stake_amount, "cannot be greater than your balance")
-    end
-  end
-  
   # Method to calculate the outcome
   def calculate_outcome(last_y_value)
     if last_y_value >= predicted_y_value
       stake_amount * predicted_y_value
     else
       0
+    end
+  end
+
+  private
+
+  def stake_amount_less_than_or_equal_to_balance
+    if stake_amount.to_i > user.balance
+      errors.add(:stake_amount, "cannot be greater than your balance")
     end
   end
 end
